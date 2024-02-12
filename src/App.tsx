da25d0469fd0264713e9de2941bf00461fdb6162
Andre Gomes <andre@appdev.com.br>
2024-02-12 10:40:03 -0300

    stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
-import "./App.css";
+import { MapTrifold } from "@phosphor-icons/react";
+import "./app.css";
+import { Logo } from "./components/logo";
+import { Button } from "./components/ui/button";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <header className="">
+        <div className=" flex flex-row bg-neutrals25 ">
+          <Logo />
+          <Button variant="default" size="lg" className=" rounded-xs">
+            default
+          </Button>
+          <Button
+            variant="secondary"
+            size="lg"
+            className="text-white rounded-xs "
+          >
+            secondary
+          </Button>
+          <Button variant="destructive" size="lg" className=" rounded-xs">
+            destructive
+          </Button>
+          <Button variant="ghost" size="lg" className=" rounded-xs">
+            <MapTrifold size={32} />
+            ghost
+          </Button>
+          <Button variant="link" size="lg" className=" rounded-xs">
+            link
+          </Button>
+          <Button variant="outline" size="lg" className=" rounded-xs">
+            outiline
+          </Button>
+        </div>
+      </header>
+      <div className="container leading-base text-primary font-bold  ">
+        asdf
+      </div>
+    </>
+  );
 }
 
 export default App;
